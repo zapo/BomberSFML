fc77bf9f7480a398fe4493ca57c48a7739d6e317
zapo <antoine.niek@supinfo.com>
2011-01-12 22:56:49 -0500

    oops, fix inclusions

--- main.cpp
+++ main.cpp
@@ -5,32 +5,26 @@
  * Created on 11 janvier 2011, 12:45
  */
 
-
 #include <cstdlib>
 #include <iostream>
 #include "Window.h"
-#include "system/events/handlers/EventHandler.h"
+#include "ClosingEventHandler.h"
+
 
 using namespace std;
-using namespace BMSfml;
 
 /*
  * 
  */
 int main(int argc, char** argv) {
 
-    // Create the main rendering window
-    Window window(800, 600, 32, "Test Window");
+	Window window(800, 600, 32, "Test Window");
 
-    ClosingEventHandler *closeHandler = new ClosingEventHandler();
+	ClosingEventHandler *closeHandler = new ClosingEventHandler();
 
-    window.addEventHandler((EventHandler*)closeHandler);
+	window.addEventHandler((EventHandler*) closeHandler);
 
+	window.run();
 
-    
-
-    // Run the main window loop
-    window.run();
-
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
